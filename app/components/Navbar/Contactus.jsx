ed43f8e47539020d56e7218a5ee7ecb77a903295
Sid9021 <sidharthkajith9021@gmail.com>
2024-11-05 00:04:10 +0530

    final

--- app/components/Navbar/Contactus.jsx
+++ app/components/Navbar/Contactus.jsx
@@ -154,7 +154,7 @@ const Contactusform = ({ isOpenForm, type }) => {
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <button
                     onClick={closeModal}  // Close button
-                    className="absolute top-4 right-4 text-2xl text-gray-500 hover:text-gray-700 focus:outline-none"
+                    className="absolute top-4 right-4 text-3xl text-gray-500 hover:text-gray-700 focus:outline-none"
                   >
                     &times; {/* Close icon */}
                   </button>
