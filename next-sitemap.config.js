56cf1472cdc139e9dc278989bfe3d0bbf8b2a1fb
Sid9021 <sidharthkajith9021@gmail.com>
2024-11-14 20:13:58 +0530

    final

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://www.smilezwellness.com/',
+    generateRobotsTxt: true, // (Optional) Generates a robots.txt file
+    // Other options: https://github.com/iamvishnusankar/next-sitemap#configuration-options
+  };
+  
